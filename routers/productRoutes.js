51c615d1f8d58298405d57107aeefdfcf945371b
fcode9 <faysalmahamud186@gmail.com>
2024-08-28 23:29:24 +0600

    add product filter route

--- routers/productRoutes.js
+++ routers/productRoutes.js
@@ -5,6 +5,7 @@ const {
   createProduct,
   updateProduct,
   getPhoto,
+  filterProducts,
 } = require("../controllers/productControllers");
 const admin = require("../middlewares/admin");
 const authorize = require("../middlewares/authorized");
@@ -12,6 +13,6 @@ const authorize = require("../middlewares/authorized");
 router.route("/").post([authorize, admin], createProduct).get(getProducts);
 router.route("/:id").put([authorize, admin], updateProduct).get(getProduct);
 router.route("/photo/:id").get(getPhoto);
-router.route("/filter").post();
+router.route("/filter").post(filterProducts);
 
 module.exports = router;
